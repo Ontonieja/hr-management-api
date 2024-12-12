8293809ffd3ccbdb4cdd1c460dde55aae2271e54
Ontonieja <ontonieja@gmail.com>
2024-12-12 15:02:07 +0100

    create tests for authentication logic

--- src/routes/user.ts
+++
@@ -1,8 +0,0 @@
-import { createUser } from "../controllers/userController";
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/create-user", createUser);
-
-export default router;
