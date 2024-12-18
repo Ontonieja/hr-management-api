095c5b70c556fd9a0401f392401932c76e332378
Ontonieja <ontonieja@gmail.com>
2024-12-18 13:55:41 +0100

    add array with company types to map them in the future

---
+++ client/src/constants/companyType.ts
@@ -0,0 +1,24 @@
+const companyTypes = [
+  { value: "TECHNOLOGY", label: "Technology" },
+  { value: "FINANCE", label: "Finance" },
+  { value: "HEALTHCARE", label: "Healthcare" },
+  { value: "EDUCATION", label: "Education" },
+  { value: "MANUFACTURING", label: "Manufacturing" },
+  { value: "RETAIL", label: "Retail" },
+  { value: "E_COMMERCE", label: "E-Commerce" },
+  { value: "CONSTRUCTION", label: "Construction" },
+  { value: "TRANSPORTATION", label: "Transportation" },
+  { value: "HOSPITALITY", label: "Hospitality" },
+  { value: "ENTERTAINMENT", label: "Entertainment" },
+  { value: "ENERGY", label: "Energy" },
+  { value: "LEGAL", label: "Legal" },
+  { value: "GOVERNMENT", label: "Government" },
+  { value: "REAL_ESTATE", label: "Real Estate" },
+  { value: "SPORTS", label: "Sports" },
+  { value: "AGRICULTURE", label: "Agriculture" },
+  { value: "MEDIA", label: "Media" },
+  { value: "NON_PROFIT", label: "Non-Profit" },
+  { value: "OTHER", label: "Other" },
+];
+
+export default companyTypes;
--- client/src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "../components/ui/button";
-
-export default function Auth() {
-  return (
-    <div className="">
-      <Button>Click me</Button>
-    </div>
-  );
-}
