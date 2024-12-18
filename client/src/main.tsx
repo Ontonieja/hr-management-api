cf25683b5ecabc138e431699a510fb9feb2edf73
Ontonieja <ontonieja@gmail.com>
2024-12-18 13:50:53 +0100

    create main with router and protected routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./pages/App.tsx";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Auth from "./pages/Auth.tsx";
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
+import Dashboard from "./pages/Dashboard.tsx";
+import ProtectedRoute from "./utils/ProtectedRoute.tsx";
 
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Navigate to="/auth" />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    ),
+  },
+]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
