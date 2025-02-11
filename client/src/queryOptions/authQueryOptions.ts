181af928a86d4f77bc5fc9a661728321a1ddadff
Ontonieja <ontonieja@gmail.com>
2025-02-11 14:28:15 +0100

    minor fixes

--- client/src/queryOptions/authQueryOptions.ts
+++ client/src/queryOptions/authQueryOptions.ts
@@ -1,15 +1,27 @@
 import api from "@/axiosConfig";
 import { queryOptions } from "@tanstack/react-query";
 
+interface AuthResponse {
+  token: string;
+  user: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    role: string;
+  };
+}
+
 export function useAuthQueryOptions() {
   return queryOptions({
     queryKey: ["use-auth"],
     queryFn: useAuth,
-    retry: 2,
+    refetchOnWindowFocus: false,
   });
 }
 
 const useAuth = async () => {
-  const { data } = await api.get("/api/v1/auth/user-info");
+  const { data } = await api.get<AuthResponse>("/api/v1/auth/user-info");
+
   return data;
 };
